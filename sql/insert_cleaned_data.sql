022e7748f01d2f4b6ba485497d8a479cb186fe17
Pokala Sai Vikram <royalsai290@gmail.com>
2025-06-12 18:59:15 +0530

    Add files via upload

---
+++ sql/insert_cleaned_data.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO cleaned_sales VALUES
+(1001, '2024-01-05', 'Alice', 'Soap', 'Bath', 2, 40.00, 80.00, 'Karnataka'),
+(1002, '2024-01-06', 'Bob', 'Shampoo', 'Hair', 1, 120.00, 120.00, 'Delhi');
